2724117614d29a972e1742f486d13fe31b5102ce
shuizhongyueming <shuizhongyueming@gmail.com>
2012-08-02 10:55:17 +0800

    the morning of day3 again

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ var http = require("http");
 var url  = require("url"); 
 function start(route,handle){
 	function onRequest (request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for "+ pathname + " received.");
-		
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		var content = route(handle,pathname);
-		response.write(content);
-		response.end();
+
+		request.setEncoding("utf8");
+
+		request.addListener("data", function(postDataChunk){
+			postData += postDataChunk;
+			console.log("received POST data chunk '" + 
+				postDataChunk + "'.");
+		});
+
+		request.addListener("end", function(){
+			route(handle, pathname, response, postData);
+		})
+
+		route(handle,pathname,response);
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
